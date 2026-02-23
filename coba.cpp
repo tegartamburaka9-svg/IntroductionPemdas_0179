cc1ea28783162ef927e4b312cd0bc87018930e04
tegartamburaka9-svg <tegartamburaka9@gmail.com>
2026-02-23 16:11:08 +0700

    first commit

---
+++ coba.cpp
@@ -0,0 +1,18 @@
+//contoh library
+#include <iostream> 
+using namespace std;
+
+//variable global 
+string nama;
+
+int main(){ //program utama
+    int age;  //varible local
+    int age1; //variable local
+    age = 20;
+    age1 = 15;
+
+    cout << "Halo World" << endl;
+    cout << 99 << endl;
+    cout << age;
+    cout << age1;
+}
